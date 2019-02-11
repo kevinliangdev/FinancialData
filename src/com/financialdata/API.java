15dcd4afdff537f3f5da2d54ffa14139ecabe2e3
Kevin Liang <kevinjiahongliang@gmail.com>
2019-02-11 17:20:57 +0800

    finished jsonparser class

---
+++ src/com/financialdata/API.java
@@ -0,0 +1,10 @@
+package com.financialdata;
+
+public class API {
+
+    private static final String API = "CS2TLFG3XZUVLWY4";
+
+    public static String getAPI() {
+        return API;
+    }
+}
--- src/com/financialdata/Main.java
+++ src/com/financialdata/Main.java
@@ -1,8 +1,24 @@
 package com.financialdata;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import static com.financialdata.parsers.JsonParser.readJsonFromUrl;
+
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException, JSONException {
+        String api = API.getAPI();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the JSON URL :");
+        String url = in.next();
+        JSONObject json = readJsonFromUrl(url);
+        String op1= json.getJSONObject("Realtime Currency Exchange Rate").getString("5. Exchange Rate");
+
+        System.out.println("Exchange rate is: " + op1);
     }
 }
